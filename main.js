5dc7bfb0fd46dbb3b815904fc8a97cbbf995755e
Andrew Huntington <andrew.huntington@gmail.com>
2021-06-22 13:18:01 +0900

    Add player name imput

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 // TODO: Separate code into different files
 
 // Factory Functions
-const player = (name) => {
+const player = (name, team) => {
   // Assign player either X or O
   // Do other player stuff here if needed
+  if (name.length === 0) {
+    team === "X" ? (name = "Player X") : (name = "Player O");
+  }
+
+  return { name, team };
 };
 
 // Modules
 const gameBoard = (() => {
-  // TODO: Remove hardcoded values
-  const currentState = ["X", "O", "X", "O", "X", "O", "O", "X", "X"];
+  // dummy values
+  // const currentState = ["X", "O", "X", "O", "X", "O", "O", "X", "X"];
+  const currentState = [
+    "&nbsp;",
+    "&nbsp;",
+    "&nbsp;",
+    "&nbsp;",
+    "&nbsp;",
+    "&nbsp;",
+    "&nbsp;",
+    "&nbsp;",
+    "&nbsp;",
+  ];
 
   return { currentState };
 })();
@@ -17,17 +33,17 @@ const gameBoard = (() => {
 const displayController = (() => {
   // Display game board to index.html
   const _boardArea = document.querySelector("#game-board");
-
   const _boardState = gameBoard.currentState;
 
   const displayBoardState = () => {
-    for (const sq of gameBoard.currentState) {
+    gameBoard.currentState.forEach((sq, index) => {
       const square = document.createElement("div");
       square.classList.add("board-square");
+      square.setAttribute("data-index", index);
 
       square.innerHTML = `<div class ="sq-text"> ${sq} </div>`;
       _boardArea.appendChild(square);
-    }
+    });
   };
 
   return { displayBoardState };
@@ -35,6 +51,15 @@ const displayController = (() => {
 
 const game = (() => {
   // Run game logic here
+  // TODO: Find another way to get player names.
+  let nameX = prompt("Please enter the name for who will be X:");
+  let nameO = prompt("Please enter the name for who will be O:");
+  const playerX = player(nameX, "X");
+  const playerO = player(nameO, "O");
+
+  console.log({ playerX });
+  console.log({ playerO });
+
   const start = () => {
     displayController.displayBoardState();
   };
