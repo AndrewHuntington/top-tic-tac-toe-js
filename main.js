b73c747a255ae09004b3f6454991bb9f333230b6
Andrew Huntington <andrew.huntington@gmail.com>
2021-06-22 19:53:32 +0900

    Begin implementing win conditions

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 // * Separate code into different files
 
 //TODO: Game Features:
-// * Show player turn on page
 // * Implement win/lose/draw conditions
+// * Include a button to start/restart the game
+// * Add display element that congratulates the winner
 // * Find alternatives to alerts/pop-ups
 // * Make play area responsive (see Snake YT video?)
 // * Find out why letters aren't centered vertically
@@ -47,11 +48,13 @@ const gameBoard = (() => {
       .firstChild.innerHTML;
 
     if (cellText !== "X" && cellText !== "O") {
-      currentState[index] = game.turn;
-      console.log({ currentState }); //TODO: REMOVE
-      displayController.displayUpdatedBoardState();
-      game.turn = game.changeTurn(game.turn);
-      displayController.displayPlayerTurn();
+      if (!game.gameOver) {
+        currentState[index] = game.turn;
+        displayController.displayUpdatedBoardState();
+        game.gameOver = game.checkWin();
+        game.turn = game.changeTurn(game.turn);
+        displayController.displayPlayerTurn();
+      }
     }
   };
 
@@ -111,6 +114,7 @@ const displayController = (() => {
 const game = (() => {
   // Run game logic here
   // TODO: Find another way to get player names.
+  let gameOver = false;
   const _nameX = prompt("Please enter the name for who will be X:");
   const _nameO = prompt("Please enter the name for who will be O:");
   const playerX = player(_nameX, "X");
@@ -118,9 +122,28 @@ const game = (() => {
   const _randomNum = Math.round(Math.random());
   let turn = _randomNum === 1 ? "X" : "O";
 
-  // TODO: For debugging. Remove later.
-  console.log({ playerX });
-  console.log({ playerO });
+  const _equalsX = (e) => e === "X";
+  const _equalsO = (e) => e === "O";
+
+  const checkWin = () => {
+    const _winCombo = {
+      topRow: gameBoard.currentState.slice(0, 3),
+      midRow: gameBoard.currentState.slice(3, 6),
+      bottomRow: gameBoard.currentState.slice(6),
+    };
+
+    for (const combo in _winCombo) {
+      if (
+        _winCombo[combo].every(_equalsX) ||
+        _winCombo[combo].every(_equalsO)
+      ) {
+        alert(`${turn === "X" ? playerX.name : playerO.name} [${turn}] WINS!`);
+        return true;
+      }
+    }
+
+    return false;
+  };
 
   const changeTurn = (playerTurn) => {
     return playerTurn === "X" ? playerO.team : playerX.team;
@@ -129,13 +152,13 @@ const game = (() => {
   const start = () => {
     //TODO: Change from using an alert
     alert(
-      `${turn === "X" ? playerX.name : playerO.name} (${turn}) goes first!`
+      `${turn === "X" ? playerX.name : playerO.name} [${turn}] goes first!`
     );
     displayController.displayPlayerInfo();
     displayController.displayInitBoardState();
   };
 
-  return { start, turn, changeTurn, playerX, playerO };
+  return { start, turn, changeTurn, playerX, playerO, checkWin, gameOver };
 })();
 
 game.start();
