257bd79b2f5ca5413d168763b613bcec1c977f77
Andrew Huntington <andrew.huntington@gmail.com>
2021-06-22 17:33:14 +0900

    Add player info to game display

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 // * Separate code into different files
 
 //TODO: Game Features:
-// * Stop players from over writing cells that already have a value
+// * Show player turn on page
 // * Implement win/lose/draw conditions
 // * Find alternatives to alerts/pop-ups
+// * Make play area responsive (see Snake YT video?)
+// * Find out why letters aren't centered vertically
 // * Add sound (Pitfall SFX?)
 // * Style (Green motif)
+// * Implement PvP and PvCPU
 
 /*********
  * BEGIN CODE *
@@ -40,11 +43,16 @@ const gameBoard = (() => {
   ];
 
   const cellClick = (index) => {
-    // const cell = document.querySelector(`[data-index="${index}"]`);
-    currentState[index] = game.turn;
-    console.log({ currentState }); //TODO: REMOVE
-    displayController.displayUpdatedBoardState();
-    game.turn = game.changeTurn(game.turn);
+    const cellText = document.querySelector(`[data-index="${index}"]`)
+      .firstChild.innerHTML;
+
+    if (cellText !== "X" && cellText !== "O") {
+      currentState[index] = game.turn;
+      console.log({ currentState }); //TODO: REMOVE
+      displayController.displayUpdatedBoardState();
+      game.turn = game.changeTurn(game.turn);
+      displayController.displayPlayerTurn();
+    }
   };
 
   return { currentState, cellClick };
@@ -54,6 +62,22 @@ const displayController = (() => {
   // Display game board to index.html
   const _boardArea = document.querySelector("#game-board");
   const _boardState = gameBoard.currentState;
+
+  const displayPlayerInfo = () => {
+    // Display player names and turn on game screen
+    const playerDisplay = document.querySelector("#player-display");
+
+    playerDisplay.innerHTML = `${game.playerX.name} Vs. ${game.playerO.name}`;
+    displayPlayerTurn();
+  };
+
+  const displayPlayerTurn = () => {
+    const playerTurn = document.querySelector("#player-turn");
+
+    playerTurn.innerHTML = `Player Turn: ${
+      game.turn === "X" ? game.playerX.name : game.playerO.name
+    } [${game.turn}]`;
+  };
 
   const displayInitBoardState = () => {
     _boardState.forEach((sq, index) => {
@@ -76,7 +100,12 @@ const displayController = (() => {
     });
   };
 
-  return { displayInitBoardState, displayUpdatedBoardState };
+  return {
+    displayInitBoardState,
+    displayUpdatedBoardState,
+    displayPlayerInfo,
+    displayPlayerTurn,
+  };
 })();
 
 const game = (() => {
@@ -84,26 +113,29 @@ const game = (() => {
   // TODO: Find another way to get player names.
   const _nameX = prompt("Please enter the name for who will be X:");
   const _nameO = prompt("Please enter the name for who will be O:");
-  const _playerX = player(_nameX, "X");
-  const _playerO = player(_nameO, "O");
-  const randomNum = Math.round(Math.random());
-  let turn = randomNum === 1 ? "X" : "O";
+  const playerX = player(_nameX, "X");
+  const playerO = player(_nameO, "O");
+  const _randomNum = Math.round(Math.random());
+  let turn = _randomNum === 1 ? "X" : "O";
 
   // TODO: For debugging. Remove later.
-  console.log({ _playerX });
-  console.log({ _playerO });
+  console.log({ playerX });
+  console.log({ playerO });
 
   const changeTurn = (playerTurn) => {
-    return playerTurn === "X" ? _playerO.team : _playerX.team;
+    return playerTurn === "X" ? playerO.team : playerX.team;
   };
 
   const start = () => {
     //TODO: Change from using an alert
-    alert(`${turn === "X" ? _nameX : _nameO} (${turn}) goes first!`);
+    alert(
+      `${turn === "X" ? playerX.name : playerO.name} (${turn}) goes first!`
+    );
+    displayController.displayPlayerInfo();
     displayController.displayInitBoardState();
   };
 
-  return { start, turn, changeTurn };
+  return { start, turn, changeTurn, playerX, playerO };
 })();
 
 game.start();
